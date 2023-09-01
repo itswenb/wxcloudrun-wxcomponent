c74011c27b38e93ee8f061f7d419ce9b50625242
Bing Wen <itswenb@qq.com>
2023-09-01 11:19:27 +0800

    Revert changes

--- comm/utils/custom.go
+++ comm/utils/custom.go
@@ -1,11 +1,24 @@
 package utils
 
 import (
+	"errors"
 	"io"
+	"os"
 
+	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/log"
 	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/wx"
 )
 
+var notificationUrl string
+
+func Init() error {
+	notificationUrl = os.Getenv("WXCOMPONENT_CALLBACK_NOTIFICATION_URL")
+	if notificationUrl == "" {
+		log.Error("notificationUrl is empty")
+		return errors.New("notificationUrl is empty")
+	}
+	return nil
+}
 func GetOriginalRequest(body io.ReadCloser) (interface{}, error) {
 	originalBody, err := io.ReadAll(body)
 	if err != nil {
